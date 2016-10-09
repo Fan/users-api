f270d541d4b65c08850f2ee76bb2e399bfb1d864
ItsDavidFan <davidfalex@gmail.com>
2016-10-09 22:01:18 +0100

    Add files via upload
    Update added, however multidimensional arrays do not work

--- users.js
+++ users.js
@@ -1,7 +1,8 @@
 var User = require('../models/user');
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var lod = require ('lodash');
 
 
 // GET /users
@@ -39,33 +40,67 @@ router.get('/:id', function(req, res) {
 // POST /users/
 // Create a user 
 router.post('/', bodyParser(), function(req, res){
-  User.create(req.body, function(err, user) {
+  User.create(req.body, function(err, user){
     if(err) { 
-      return handleError(res, err); 
+      return res.status(500).json({
+        error: "Error creating user: " + err
+      });
     }
     return res.status(201).json(user);
-    console.log(req.body);
   });
 });
 
 // DELETE /users/:id
 // Delete a user by id
-router.delete('/:id', function(req, res) {
-  User.findById(req.params.id, function (err, user) {
+router.delete('/:id', function(req, res){
+  User.findById(req.params.id, function (err, user){
     if(err) { 
-      return handleError(res, err); 
+      return res.status(404).json({
+        error: "Error deleting user: " + err
+      });
     }
     if(!user) { 
       return res.status(404).send('Not Found'); 
     }
-    user.remove(function(err) {
+    user.remove(function(err){
       if(err) { 
-        return handleError(res, err); 
+        return res.status(404).json({
+        error: "Error deleting user: " + err
+        });
       }
       return res.status(204).send('No Content');
     });
   });
 });
+
+// PUT /users/:id
+// Update a user by id 
+// key:{key:value} not updating/ multidimensional arrays not updating
+router.put('/:id', bodyParser.json(), function(req, res){
+  User.update(req.body, function(err, user){
+    User.findById(req.params.id, function(err, user){
+      if (err){
+        return res.status(404).json({
+        error: "Error updating user: " + err
+      });
+      }
+      if (!user){
+        return res.status(404).send('User not Found');
+      }
+      var updated = lod.merge(user, req.body);
+      updated.save(function(err){
+        if (err){
+          return res.status(404).json({
+          error: "Error updating user: " + err
+          });
+        }
+      return res.status(200).json(user);
+      });
+    });
+  });
+});
+
+
 
 
 
